75db5f5ebefc1449eb2180792492321ffae57041
Bethel-21 <bethelhemdanielfenta@gmail.com>
2025-10-14 21:58:06 +0300

    chore: init project, create core and feature apps, add DRF & SimpleJWT

--- be_er/be_er/settings.py
+++ be_er/be_er/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
     'rest_framework',
     'users',
     'posts',
@@ -128,3 +129,24 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
+
+
+
+
+
+
+
+
+
+AUTH_USER_MODEL = 'users.User'
