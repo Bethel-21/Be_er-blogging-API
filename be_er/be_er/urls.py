ade556e680cba2e7bebe2aa9683f3150086ac8d8
Bethel-21 <bethelhemdanielfenta@gmail.com>
2025-10-14 22:09:05 +0300

    feat(api): register routers and JWT auth endpoints

--- be_er/be_er/urls.py
+++ be_er/be_er/urls.py
@@ -1,26 +1,22 @@
-"""
-URL configuration for be_er project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# be_er/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from users.views import UserViewSet
+from categories.views import CategoryViewSet
+from posts.views import PostViewSet
+from comments.views import CommentViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
+router.register(r'categories', CategoryViewSet)
+router.register(r'posts', PostViewSet)
+router.register(r'comments', CommentViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
-    path('posts/', include('posts.urls')),
-    path('categories/', include('categories.urls')),
-    path('comments/', include('comments.urls')),
+    path('api/', include(router.urls)),
+    path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
